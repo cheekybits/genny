50813d4bd60f75ac5db7d0bd5ca9d7f45fba5a41
Mat Ryer <matryer@Mats-MacBook-Pro.local>
2014-10-30 18:18:08 -0500

    tweaked usage

--- main.go
+++ main.go
@@ -77,10 +77,13 @@ gen - generates type specific code (to stdout) from generic code (via stdin) or 
 
 {types}  - (required) Specific types for each generic type in the source
 {types} format:  {generic}={specific}[,another][ {generic2}={specific2}]
+
 Examples:
   Generic=Specific
   Generic1=Specific1 Generic2=Specific2
   Generic1=Specific1,Specific2 Generic2=Specific3,Specific4
+
+Flags:
 `)
 	flag.PrintDefaults()
 }
